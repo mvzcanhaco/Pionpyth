1040cf96e6d0c44f63fd4ffd3b61ee8ad75e99f2
Marcus Canhaco <marcuscanhaco@gmail.com>
2015-04-11 23:38:46 -0300

    First commit test

---
+++ Programa1/src/programa1.py
@@ -0,0 +1,21 @@
+'''
+Created on 08/04/2015
+
+@author: Marcus
+'''
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# programa1.py - Primeiro Programa
+
+import random
+numero = random.randint(1,100)
+escolha = 0
+tentativas = 0
+while escolha != numero:
+    escolha = input("Escolha um numero entre 1 a 100:")
+    tentativas +=1
+    if escolha < numero:
+        print "O numero", escolha, "e menor que o sorteado."
+    elif escolha > numero:
+        print "O numero", escolha, "e maior que o sorteado"
+print "Parabens! Voce acertou com", tentativas, "tentativas."
---
+++ exemplo/src/HelloWorld.py
@@ -0,0 +1,25 @@
+'''
+Created on 06/04/2015
+
+@author: Marcus
+'''
+
+
+import Tkinter
+from time import strftime
+relogio = Tkinter.Label()
+relogio.pack()
+relogio['font'] = 'Helvetica 120 bold'
+relogio['text'] = strftime('%H:%M:%S')
+def tictac():
+    agora = strftime('%H:%M:%S')
+    if agora != relogio['text']:
+        relogio['text'] = agora
+    relogio.after(100, tictac)
+
+
+if __name__ == '__main__':
+    variavel = 'Hello World'
+    print variavel
+    tictac()
+    relogio.mainloop()
